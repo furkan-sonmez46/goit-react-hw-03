287c325f8ae4d27c365f24a9e3c5102e59e25b74
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-03-25 22:31:39 +0300

    components files found

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useId, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
+    </>
+  );
+}
+
+export default App
